2ba18376e2eb705eff59bd6bb4947bd0839d73af
ihtisham914 <shaami.khn321@gmail.com>
2023-12-11 18:34:03 +0500

    updated

--- src/components/MeetingRecord.jsx
+++ src/components/MeetingRecord.jsx
@@ -214,14 +214,6 @@ const MeetingRecord = () => {
 
   return (
     <div className="flex items-center gap-2">
-      <input
-        value={meetingUrl}
-        onChange={(e) => setMeetingUrl(e.target.value)}
-        type="text"
-        placeholder="Enter Meeting URL"
-        className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 w-[150px]"
-      />
-
       {/* Record Button */}
       {inMeeting === "Error" && isRecording ? (
         <div className="flex items-center gap-1">
@@ -242,26 +234,35 @@ const MeetingRecord = () => {
           )}
         </div>
       ) : (
-        <button
-          onClick={() => {
-            handleAddBotClick();
-            startRecording();
-            UploadFile();
-          }}
-          className="bg-blue-500 text-white px-4 py-2 flex rounded-md focus:outline-none hover:bg-blue-600"
-        >
-          {inMeeting === "started" ? (
-            <div className="mr-1 mt-1 animate-spin">
-              <AiOutlineLoading3Quarters />
-            </div>
-          ) : (
-            <div className="mr-1 mt-1">
-              <MdOutlineKeyboardVoice />
-            </div>
-          )}
+        <>
+          <input
+            value={meetingUrl}
+            onChange={(e) => setMeetingUrl(e.target.value)}
+            type="text"
+            placeholder="Enter Meeting URL"
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 w-[150px]"
+          />
+          <button
+            onClick={() => {
+              handleAddBotClick();
+              startRecording();
+              UploadFile();
+            }}
+            className="bg-blue-500 text-white px-4 py-2 flex rounded-md focus:outline-none hover:bg-blue-600"
+          >
+            {inMeeting === "started" ? (
+              <div className="mr-1 mt-1 animate-spin">
+                <AiOutlineLoading3Quarters />
+              </div>
+            ) : (
+              <div className="mr-1 mt-1">
+                <MdOutlineKeyboardVoice />
+              </div>
+            )}
 
-          <div>Record</div>
-        </button>
+            <div>Record</div>
+          </button>
+        </>
       )}
     </div>
   );
